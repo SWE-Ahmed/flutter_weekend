5721b91b81e282701b26599cd222af48bb6f8ea2
Ahmed Almohammed <master.necro15@gmail.com>
2021-11-22 22:11:45 +0300

    modified the splash screen with extra methods

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -5,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weekend_app/constants.dart';
 import 'package:flutter_weekend_app/first_sign_in_form.dart';
 import 'package:flutter_weekend_app/home_screen.dart';
+import 'package:flutter_weekend_app/personal.dart';
 import 'package:flutter_weekend_app/sign_in_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -15,20 +18,59 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  bool _haveAccount = false;
+  final bool _haveAccount = false;
+
   final user = FirebaseAuth.instance.currentUser;
 
-  // the following method checks for the user email in the firebase cloud
-  Future<void> checkEmail() async {
-    // creates an instance of the collection on the firebase
+  Widget checkEmail() {
     CollectionReference collection =
         FirebaseFirestore.instance.collection("users");
-    // creates an instance of the documents in the speciifed collection
-    DocumentSnapshot snapShot = await collection.doc(user.uid).get();
-    // if the user uinqe id is registered on firebase, the value of have account will change to truu
-    if (snapShot.get('email').toString().compareTo(user.email) == 0) {
-      _haveAccount = !_haveAccount;
-    }
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: collection.doc(user.uid).get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data.data() as Map<String, dynamic>;
+          return Text("${data['email']}");
+        }
+        return Text("Loading");
+      },
+    );
+  }
+
+  Widget nextScreen() {
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            // user is still in sigining in process
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+              // user is signed in successfully
+            } else if (snapshot.hasData) {
+              // if the user already has an account
+              checkEmail();
+              if (_haveAccount) {
+                return HomeScreen();
+                // the user's unique id will be used as the name of the document in firebase
+              } else {
+                CollectionReference usersInfo =
+                    FirebaseFirestore.instance.collection('users');
+                usersInfo.doc(user.displayName).set({'email': user.email});
+                // adds the user's email to the firebase firestore
+                return HomeScreen();
+              }
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Something went wrong"));
+            } else {
+              return SignInGoogle();
+            }
+          }),
+    );
   }
 
   @override
@@ -38,7 +80,7 @@ class _SplashScreenState extends State<SplashScreen> {
         splashIconSize: 400,
         splash: splash,
         backgroundColor: splashColor,
-        nextScreen: AccountCheck(haveAccount: _haveAccount, user: user),
+        nextScreen: nextScreen(),
         centered: true,
         splashTransition: SplashTransition.rotationTransition,
         duration: 3000,
@@ -47,7 +89,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
-class AccountCheck extends StatelessWidget {
+/* class AccountCheck extends StatelessWidget {
   const AccountCheck({
     Key key,
     @required bool haveAccount,
@@ -89,4 +131,4 @@ class AccountCheck extends StatelessWidget {
           }),
     );
   }
-}
+} */
